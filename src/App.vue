ba62f906ebab5926ef99f413935fdbbb40fe646f
pasan04 <pasankamburugamuwa1@gmail.com>
2022-07-05 10:48:31 +0530

    Add emitting custom events through emits

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
     <div class="parent-component">
-      <child-component message="Component Communication in VueJs"></child-component>
+      <child-component @changeMsg="setMessage" :msg="welcomeMsg"></child-component>
     </div>
 </template>
 
 <script>
 
 export default {
+  data() {
+    return {
+      welcomeMsg: "Hello World"
+    };
+  },
+  methods: {
+    setMessage(msg) {
+      this.welcomeMsg = msg;
+    }
+  },
 
 }
 </script>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,11 +1,26 @@
 <template>
-  <h1>{{message}}</h1>
+  <h1 class="is-size-3">{{ msg }}</h1>
+  <br>
+  <input class="input" type="text" v-model="changeMsgVar"> <br/><br/>
+  <button @click="changeMsg"> Change Message
+  </button>
 </template>
 
 <script>
 export default {
+  data(){
+      return {
+        changeMsgVar:''
+      }
+  },
   props: {
-    message: String
+    msg: String
+  },
+  methods:{
+    changeMsg(){
+      this.$emit("changeMsg", this.changeMsgVar);
+      console.log('message emit from child component');
+    }
   }
 }
 </script>
